f3d6c1a95889c8020ae0e3e28fe221f1bb9dfc15
Jusanov <justinhschaaf@outlook.com>
2018-09-18 20:38:02 -0700

    Add Content
    Add the styles that I already have.

---
+++ getcolor.js
@@ -0,0 +1,7 @@
+// Test script
+/*function getColor(divId) {
+    
+    divObj = document.getElementById(divId);
+    alert ( "The color for this element is " + window.getComputedStyle(divObj).getPropertyValue('background-color') ); //divObj.backgroundColor
+    
+}*/
